0951dc6f056f70935a7bb209aefe487a544f8a67
German <gerbisu@gmail.com>
2023-08-01 16:28:21 -0300

    Videogames-1.5

--- api/src/controllers/videogamesControllers.js
+++ api/src/controllers/videogamesControllers.js
@@ -1,7 +1,7 @@
 //El controller es el que hace la interacion con fuentes externas de informacion (Conexion con base de datos)
 const axios = require("axios");
 const {Videogame} = require("../db")
-const {API_KEY,URL_GENRES} = process.env;
+const {API_KEY,URL_GAMES} = process.env;
 
 const createVideogameDB = async (name,description,platforms,image,released,rating,genre) => {
     
@@ -10,15 +10,27 @@ const createVideogameDB = async (name,description,platforms,image,released,ratin
 }
 
 const AllGames = async () => {
-    
+
 }
 
-const VideogameById = async () => {
+const VideogameById = async (id, dataLocation) => {
 
+    let videogameById;
+
+    if(dataLocation === "api"){//Reviso donde buscar el videogame
+        const response = await axios.get(`${URL_GAMES}/${id}?key=${API_KEY}`);//Hago peticion a la API
+        const { id: apiId, name, description, released, platforms, background_image, rating, genres } = response.data;//me quedo con los datos que me interesan
+        const platformsName = platforms.map(data => data.platform.name);//Me quedo solo con el nombre
+        const genresName = genres.map(data => data.name); //Me quedo solo con el nombre
+        videogameById = { id, name, description, released, platformsName, background_image, rating, genresName };//Guardo la info
+    } else {
+        videogameById = await Videogame.findByPk(id);
+    }
+    return videogameById;
 }
 
 const VideogameByName = async () => {
-    
+
 }
 
 module.exports = {
